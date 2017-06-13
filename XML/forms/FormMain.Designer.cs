c1d5d09528a625d451aca65baf63683a17efbb87
Alexey Khrusch <alexeykhr@outlook.com>
2017-06-13 09:05:40 +0300

    Fix comboBox category

--- XML/forms/FormMain.Designer.cs
+++ XML/forms/FormMain.Designer.cs
@@ -483,6 +483,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(325, 21);
             this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
