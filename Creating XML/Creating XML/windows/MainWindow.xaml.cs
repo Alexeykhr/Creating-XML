8271d8079912ab2db81be725e24df5f72e2d7301
Alexey Khrushch <alexeykhr@outlook.com>
2018-02-10 18:30:00 +0200

    MainWindow - updated UI (added paginate), added some code (previewTextInput, other)

--- Creating XML/Creating XML/windows/MainWindow.xaml.cs
+++ Creating XML/Creating XML/windows/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,6 +21,9 @@ namespace Creating_XML.windows
 {
     public partial class MainWindow : Window
     {
+        private int page = 1;
+        private int count = 20;
+
         /// <summary>
         /// Select file before work (open window).
         /// </summary>
@@ -28,6 +32,7 @@ namespace Creating_XML.windows
         {
             InitializeComponent();
             OpenFileWindow();
+            UI();
         }
 
         /// <summary>
@@ -49,6 +54,55 @@ namespace Creating_XML.windows
                 Close();
             else
                 Show();
+        }
+
+        /// <summary>
+        /// Update UI (Fill data).
+        /// </summary>
+        private void UI()
+        {
+            fMaxItemsOnPage.Text = count.ToString();
+            fCurrentPage.Text = page.ToString();
+
+            var list = Database.List<OfferTable>();
+            // TODO
+        }
+
+        /// <summary>
+        /// After got focus = clear the text.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fCurrentPage_GotFocus(object sender, RoutedEventArgs e)
+        {
+            fCurrentPage.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Check Input.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fCurrentPage_LostFocus(object sender, RoutedEventArgs e)
+        {
+            bool isCorrect = int.TryParse(fCurrentPage.Text, out int page);
+
+            if (!isCorrect || page < 1)
+                page = this.page;
+
+            fCurrentPage.Text = page.ToString();
+            this.page = page;
+        }
+
+        /// <summary>
+        /// Only accept numbers.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fCurrentPage_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
         /*
